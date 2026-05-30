0afe0df1c8818040ef3fda16bbcb1bacbcd0cd2e
Enrico <enricodeabm@hotmail.com>
2026-05-30 07:53:29 -0300

    feat: Acionamento dos atuadores

---
+++ actuators.py
@@ -0,0 +1,33 @@
+from machine import Pin
+from time import sleep_us
+
+class Motor:
+    def __init__(self, direc, step):
+        self.direc = Pin(direc, Pin.OUT)
+        self.step = Pin(step, Pin.OUT)
+
+    def move(self, direc, steps, delay):
+        self.direc.value(direc)
+        steps = abs(steps)
+    
+        for i in range(steps):
+            self.step.value(1)
+            sleep_us(delay)
+            self.step.value(0)
+            sleep_us(delay)
+
+class Actuator:
+    def __init__(self, pin, name):
+        self.pin = Pin(pin, Pin.OUT)
+        self.name = name
+
+    def on(self):
+        self.pin.value(1)
+
+    def off(self):
+        self.pin.value(0)
+
+    def toggle(self):
+        self.pin.value(
+            not self.pin.value()
+        )
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # ========= OLED =========
-#  - SDA OLED
-#  - SCL OLED
+# 21 - SDA OLED
+# 16 - SCL OLED
 
 # ========= FOGÃO =========
 # 34 - Termostato - ADC
-#  - Valvula Solenoide
+# 17 - Valvula Solenoide
 
 # ========= JANELA =========
 # 32 - TRIGGER - DIG
 # 35 - ECHO - DIG
-#  - STEP
+# 26 - STEP
 # 22 - DIR
 
 # ========= FECHADURA =========
@@ -29,6 +29,8 @@
 
 
 from sensors import Thermostat, Ultrassonic, PIR, MFRC522, Button
+from actuators import Motor, Actuator
+from display import Display
 from time import sleep
 
 temp_sensor = Thermostat(34)
@@ -37,11 +39,31 @@ pir = PIR(33)
 rfid = MFRC522()
 btn = Button(25)
 
-print("Iniciado...")
+motor = Motor(direc = 22, step = 26)
+
+valv = Actuator(pin = 17, name = "Válvula Solenoide")
+lock = Actuator(pin = 14, name = "Fechadura")
+buzzer = Actuator(pin = 12, name = "Buzzer")
+led = Actuator(pin = 13, name = "LED de Alarme")
+
+oled = Display(sda = 21, scl = 16)
+
+print("Iniciando...")
 while True:
-    # print(f"Temperatura: {temp_sensor.calc()}")
-    # print(f"Distância: {ultrassonic.measure()} cm")
-    # print(f"Presença: {'Tem movimento.' if pir.hasMovement() else 'Sem movimento'}")
-    # print("card: " + rfid.read())
-    print("botão: " + str(btn.read()))  
+    print(f"Temperatura: {temp_sensor.calc()}")
+    print(f"Distância: {ultrassonic.measure()} cm")
+    print(f"Presença: {'Tem movimento.' if pir.hasMovement() else 'Sem movimento'}")
+    print("card: " + rfid.read())
+    print("botão: " + str(btn.read()))
+
+    motor.move(1, 200, 2000)
+
+
+    valv.toggle()
+    lock.toggle()
+    led.toggle()
+    buzzer.toggle()
+      
+    oled.show("")
+
     sleep(1)
